f9c55f4430caeb1f6cd6b78e1f689592b1d55810
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-09-18 15:55:42 +0530

    Update deployToEKS.groovy

--- vars/deployToEKS.groovy
+++ vars/deployToEKS.groovy
@@ -6,10 +6,21 @@ def call(
     String manifest
 ) {
 
+    def serviceManifest = manifest.replace(
+        '-deployment.yaml',
+        '-service.yaml'
+    )
+
     sh """
         kubectl apply \
             -f ${manifest} \
             -n ${namespace}
+
+        if [ -f "${serviceManifest}" ]; then
+            kubectl apply \
+                -f ${serviceManifest} \
+                -n ${namespace}
+        fi
 
         kubectl -n ${namespace} set image \
             deployment/${deploymentName} \
